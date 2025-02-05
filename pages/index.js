1a91fd455f49e5cf26fb8289298b53d6e898f7ec
NicolasM.L <104863592+NicolasMagarinus@users.noreply.github.com>
2025-02-05 13:52:37 +0000

    Página de construção e arquivos de manifesto

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div align="center">
+      <style>
+        {`
+          body {
+            background-color: #b5edff;
+          }
+        `}
+      </style>
+      <h1>
+        Projeto Releitura.com.br: um local para opiniões de livros, quadrinhos,
+        revistas e outros tipos de histórias em papéis!
+      </h1>
+      <h2>Em construção... 🔨 🔨</h2>
+    </div>
+  );
+}
+
+export default Home;
